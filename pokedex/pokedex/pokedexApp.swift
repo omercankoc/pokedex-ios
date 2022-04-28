4efd3747e0a453c58bec396a9882fa009543f0db
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2022-04-28 13:12:29 +0300

    Created App

---
+++ pokedex/pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Ömer Can Koç on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
