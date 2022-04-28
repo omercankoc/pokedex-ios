1095c85efb85c4cb1b14f95a02ea818e2655b99a
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2022-04-28 14:40:49 +0300

    Get pokemon"s"

---
+++ pokedex/pokedex/Service/PokemonSelected.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct PokemonSelected : Codable {
+    var sprites : PokemonSprites
+    var weight : Int
+}
+
+struct PokemonSprites : Codable {
+    var front_defaults : String?
+}
+
+class PokemonSelectedApi {
+    func getData(url : String, completion : @escaping (PokemonSprites) -> ()){
+        guard let url = URL(string : url) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url){(data,response,error) in
+            guard let data = data else { return }
+            let pokemonSprite = try! JSONDecoder().decode(PokemonSelected.self, from: data)
+            DispatchQueue.main.async {
+                completion(pokemonSprite.sprites)
+            }
+        }.resume()
+    }
+}
